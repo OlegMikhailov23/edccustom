b7182029903bcaff370fbbe4090e29a3471c35d4
Dkfolkin <dkfolkin@gmail.com>
2018-09-25 18:11:03 +0300

    подключаем слайдер

--- source/js/owl.js
+++ source/js/owl.js
@@ -1,8 +1,16 @@
-document
+
 
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
-    dots:true
+    dots:false,
+    items:1,
+    autoplay: true,
+    nav: false,
+    autoplaySpeed: 1000,
+    autoplayTimeout: 10000,
+    mouseDrag: false,
+    touchDrag: false,
+    margin: 0
 });
